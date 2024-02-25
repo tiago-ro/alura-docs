6ffbfab1d0119506a83ba8ec4b1a0ba33844d1ab
tiago-ro <tiago.ro.dev@gmail.com>
2024-02-25 07:05:44 -0300

    server created

---
+++ src/server.js
@@ -0,0 +1,13 @@
+import express from "express";
+import url from "url";
+import path from "path";
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+const caminhoAtual = url.fileURLToPath(import.meta.url);
+const diretorioPublico = path.join(caminhoAtual, "../..", "public");
+app.use(express.static(diretorioPublico));
+
+
+app.listen(PORT, () => console.log(`Servidor escutando na porta ${PORT}`)); 
