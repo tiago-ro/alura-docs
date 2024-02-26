a37d201d5163ec3f0de8be473f16e751358747c8
tiago-ro <tiago.ro.dev@gmail.com>
2024-02-26 08:26:47 -0300

    integrating socket.io

---
+++ public/document.js
@@ -0,0 +1,1 @@
+const soocket = io();
--- src/server.js
+++ src/server.js
@@ -1,6 +1,8 @@
 import express from "express";
 import url from "url";
 import path from "path";
+import http from "http";
+import { Server } from "socket.io";
 
 const app = express();
 const PORT = process.env.PORT || 3000;
@@ -9,5 +11,13 @@ const caminhoAtual = url.fileURLToPath(import.meta.url);
 const diretorioPublico = path.join(caminhoAtual, "../..", "public");
 app.use(express.static(diretorioPublico));
 
+const serverHttp = http.createServer(app);
 
-app.listen(PORT, () => console.log(`Servidor escutando na porta ${PORT}`)); 
+serverHttp.listen(PORT, () => console.log(`Servidor escutando na porta ${PORT}`)); 
+
+const io = new Server(serverHttp);
+
+io.on("connection", () => {
+    console.log("Um cliente se conectou");
+});
+
